ed6e3ff287f5dd9ad5ce57990ccb73bab595d60a
zeroclock <zeroclock@icloud.com>
2020-11-13 23:09:03 +0900

    coverage and travis

--- coverage.sh
+++ coverage.sh
@@ -6,11 +6,13 @@ PROJ_NAME=$(cat Cargo.toml | grep -E "^name" | sed -E 's/name[[:space:]]=[[:spac
 rm -rf target/debug/deps/${PROJ_NAME}-*
 
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off"
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -C panic=abort"
 
-cargo +nightly build --verbose
-cargo +nightly test --verbose
+cargo +nightly build
+cargo +nightly test
 
-grcov ./target/debug/deps -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" --excl-br-start "mod tests \{" --excl-start "mod tests \{" --excl-line "#\[" --excl-br-line "#\[" -o lcov.info
-genhtml -o report/ --show-details --highlight --ignore-errors source --legend lcov.info --branch-coverage
+zip -0 ccov.zip `find . \( -name "${PROJ_NAME}*.gc*" -o -name "test-*.gc*" \) -print`
+grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" --ignore "tests/*" -o lcov.info
+rust-covfix -o lcov_correct.info lcov.info
+genhtml -o report/ --show-details --highlight --ignore-errors source --legend lcov_correct.info --branch-coverage
 
